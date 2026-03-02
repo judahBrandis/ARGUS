0d78e2ae0a82386cacd58f0a00c56fbf578b0606
Judah <71202523+judahBrandis@users.noreply.github.com>
2026-03-02 14:18:42 -0500

    Delete lib/ZMPT101Bj directory
    These libraries are no longer needed as I'm now using a voltage divider instead of this.

--- lib/ZMPT101Bj/ZMPT101Bj.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// The two functions I created in my ZMPT101Bj library
-void measureVoltageSetup(); // Used in setup, deployed once
-float measureVoltage(); // Stores the voltage and displays in seriole output
