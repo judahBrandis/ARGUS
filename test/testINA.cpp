881fcd1eccf4910de5446126799c06e0f17790d1
Judah <71202523+judahBrandis@users.noreply.github.com>
2026-03-23 17:45:34 -0400

    v0.2.0 - INA sensor works
    The INA219 sensor now measures voltage, current, and power. It does not measure resistance at this time but adding this feature to the code would not be difficult at all if need be.

--- test/testINA.cpp
+++ test/testINA.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
-#include <currentSensor.h>
+#include <INA219j.h>
 
 void setup () {
     Serial.begin(9600);
 
-    measureCurrentSetup(); // sets up the INA sensor
+    setupINA(); // sets up the INA sensor
 }
 
 void loop () {
-    measureVoltageINA(); // measures voltage from the INA sensor
-    measureCurrent(); // measures current from the INA sensor
-    calculatePower(measureCurrent(), measureVoltageINA()); // Calculates power 
+    calculatePower(measureVoltage(), measureCurrent()); // Calculates power 
+    Serial.println("");
 
-    delay(1000); // Pauses for 1 second before continuing
+    delay(2000); // Pauses for 1 second before continuing
 }
