3025a30511b22609d3dea75501c397874fd1c11e
Judah <71202523+judahBrandis@users.noreply.github.com>
2026-03-24 20:49:14 -0400

    Delete lib/voltageSensor directory

--- lib/voltageSensor/voltageSensor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <arduino.h>
-
-// The two functions I created in my voltage sensor library
-void measureVoltageSetup(uint8_t voltagePin); // Used in setup, deployed once
-float measureVoltage(uint8_t voltagePin, float voltageTune); // Stores the voltage and displays in seriole output
