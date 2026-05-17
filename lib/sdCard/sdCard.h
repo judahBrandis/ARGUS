57d3976406ed5e5c1ecb3649086a0330917dace4
Judah <71202523+judahBrandis@users.noreply.github.com>
2026-05-17 11:50:08 -0400

    Revert commit
    Previous commit was problematic.

--- lib/sdCard/sdCard.h
+++ lib/sdCard/sdCard.h
@@ -1,5 +1,3 @@
 #pragma once
 
-// The two functions I created in my BMP280 library
-void setupSD(); // Used in setup, deployed once
-void storeSD (float time, float volt, float current, float temp); // Stores data in the sd card
+#include <arduino.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
 #include <Arduino.h>
-#include <sdCard.h>
 #include <INA219j.h>
-#include <BMP280.h>
-
-#define measureTime (1) // until I can measure time accurately
 
 void setup () {
     Serial.begin(9600);
 
-    measureTemperatureSetup(); // sets up the temperature measuring function
     setupINA(); // sets up the INA sensor
-    setupSD(); // sets up the INA sensor
 }
 
 void loop () {
-    // read and log timestamp, current, voltage, and temperature
-    storeSD(measureTime, measureVoltage(), measureCurrent(), readTemperature()); 
-    delay(1000);
+    calculatePower(measureVoltage(), measureCurrent()); // Calculates power 
+    Serial.println("");
+
+    delay(2000); // Pauses for 1 second before continuing
 }
